4a220a19958cd501ad76067904f554b2e89d1d1f
Tim Foley <thetimfoley@gmail.com>
2016-10-17 09:43:15 -0400

    generate controllers, fixed seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 User.destroy_all
 Expense.destroy_all
+Debt.destroy_all
 
 tim = User.create!(name: "Tim", email: "tim@tim.tim")
 jack = User.create!(name: "Jack", email: "jack@jack.jack")
@@ -16,3 +17,6 @@ matt = User.create!(name: "Matt", email: "matt@matt.matt")
 tim.expenses.create!(name: "Paper Towels", notes: "NA", date: Date.today, amount: 30, share: 10)
 jack.expenses.create!(name: "Rent", notes: "NA", date: Date.today, amount: 3000, share: 1000)
 matt.expenses.create!(name: "Netflix", notes: "NA", date: Date.today, amount: 9, share: 3)
+
+tim.debts.create!(amount: 1000, reconciled: false, expense_id: jack.expenses.first.id)
+tim.debts.create!(amount: 3, reconciled: false, expense_id: matt.expenses.first.id)
