59e78a0032eaa47e2b71b94f81d5013a8d60ad75
Tim Foley <thetimfoley@gmail.com>
2016-10-17 09:57:22 -0400

    basic controller scaffolding

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,46 @@
 class ExpensesController < ApplicationController
+
+  # replace Expense
+  # replace expense
+  def index
+    @expenses = Expense.all
+  end
+
+  def show
+    @expense = Expense.find(params[:id])
+  end
+
+  def new
+    @expense = Expense.new
+  end
+
+  def create
+    @expense = Expense.create(expense_params)
+
+    redirect_to expense_path(@expense)
+  end
+
+  def edit
+    @expense = Expense.find(params[:id])
+  end
+
+  def update
+    @expense = Expense.find(params[:id])
+    @expense.update(expense_params)
+
+    redirect_to expense_path(@expense)
+  end
+
+  def destroy
+    @expense = Expense.find(params[:id])
+    @expense.destroy
+
+    redirect_to expenses_path
+  end
+
+  # strong params
+  private
+  def expense_params
+    params.require(:expense).permit(:amount, :reconciled, :expense_id, :user_id)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,45 @@
 class UsersController < ApplicationController
+  # replace User
+  # replace user
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    redirect_to user_path(@user)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to users_path
+  end
+
+  # strong params
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
