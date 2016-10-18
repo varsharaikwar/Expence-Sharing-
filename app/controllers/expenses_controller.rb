434ae0864f8c3918c8a9436b522083dc7e030c91
Tim Foley <thetimfoley@gmail.com>
2016-10-18 09:29:54 -0400

    tweaked views

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -17,10 +17,12 @@ class ExpensesController < ApplicationController
   def create
     @expense = Expense.new(expense_params)
     @expense.user_id = current_user.id
-    # @expense.reconciled = false;
     @expense.save
 
-    redirect_to expense_path(@expense)
+    # Should I create the debt here?
+    @debt = Debt.new()
+
+    redirect_to user_path(current_user)
   end
 
   def edit
@@ -44,6 +46,9 @@ class ExpensesController < ApplicationController
   # strong params
   private
   def expense_params
+    params.require(:expense).permit(:amount, :reconciled, :expense_id, :user_id, :date, :notes, :share, :name)
+  end
+  def debt_params
     params.require(:expense).permit(:amount, :reconciled, :expense_id, :user_id, :date, :notes, :share, :name)
   end
 end
